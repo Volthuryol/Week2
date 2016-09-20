d1bdc8387a48adbf1c36df013685e00bbbfedf46
Volthuryol <caden.cheek@interapthq.com>
2016-09-20 16:50:30 -0400

    Week2

--- Day1/JQueryChallenge.js
+++ Day1/JQueryChallenge.js
@@ -1,10 +1,12 @@
-$function(){
-  $("#add").click(function(ev) {
+$(function(){
+  $("#add").click(function() {
     var superhero = $("#superhero").val();
-
+    
     if (!superhero) return; 
-    $("#list").append("<li>" + superhero + "</li>");
-
-    $("#superhero").val(" ");
+    $("#list").append("<li>" + superhero + "</li>" + "<button class='remove'>(x)</button>");
+    $("#superhero").val("");
+    $(".remove").click(function(ev){
+      $(ev.target).parent().fadeOut();
+    });
   });
-};
+});
